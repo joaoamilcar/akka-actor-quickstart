4286e2c1f0ec36ff761587b4b18310d566d93bf5
João Amílcar <joaoamilcar@gmail.com>
2018-02-22 01:21:11 -0300

    unknown message received

--- src/main/scala/actors/SimpleActor.scala
+++ src/main/scala/actors/SimpleActor.scala
@@ -8,7 +8,10 @@ class SimpleActor extends Actor {
   def receive = {
     case StringMessage(phrase) => println("text: " + phrase)
     case IntegerMessage(number) => println("number: " + number)
-    case _ => context.stop(self)
+    case _ => {
+      println("unknown message received")
+      context.stop(self)
+    }
   }
 
   def foo = print("inner method")
